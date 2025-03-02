6d280af039ddae63ac5536f6ec4b7db4ff4e63d7
skyclouds2001 <skyclouds2001@163.com>
2025-03-02 15:57:06 +0800

    style: move assets files

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 /* global __BUILD_DATE__ */
 
 import sum from '@/sample'
-import list from '@/sample.json'
-import values from '@/sample.yaml'
+import list from '@/assets/sample.json'
+import values from '@/assets/sample.yaml'
 // eslint-disable-next-line import/no-unresolved
 import num from 'virtual-module'
 
